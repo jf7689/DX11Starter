6b57b9484a47717caab3d85d2f849206f25ac4e2
jf7689 <jf7689@rit.edu>
2022-01-20 17:11:53 -0500

    Mesh class started

---
+++ Mesh.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <d3d11.h>
+#include <wrl/client.h>
+#include "Vertex.h"
+
+class Mesh
+{
+public:
+	Mesh(Vertex* _vertices, int _nVertices, unsigned int* _indices, int _nIndicies, 
+		Microsoft::WRL::ComPtr<ID3D11Device> _device, Microsoft::WRL::ComPtr<ID3D11DeviceContext> _context);
+	~Mesh();
+
+	Microsoft::WRL::ComPtr<ID3D11Buffer> GetVertexBuffer();
+	Microsoft::WRL::ComPtr<ID3D11Buffer> GetIndexBuffer();
+	int GetIndexCount();
+
+private:
+	Microsoft::WRL::ComPtr<ID3D11Buffer> vb;
+	Microsoft::WRL::ComPtr<ID3D11Buffer> ib;
+
+	// number of indicies
+	int nIndicies;
+
+};
