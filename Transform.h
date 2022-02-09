38385929603a07c8d6cbad0f271e39c769e8163c
jf7689 <jf7689@rit.edu>
2022-02-08 19:42:35 -0500

    Transform header setup

---
+++ Transform.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <DirectXMath.h>
+
+class Transfrom
+{
+public:
+	Transfrom();
+	void MoveAbsolute(float x, float y, float z);
+	void Rotate(float p, float y, float r);
+	void Scale(float x, float y, float z);
+
+	void SetPosition(float x, float y, float z);
+	void SetRotation(float p, float y, float r);
+	void SetScale(float x, float y, float z);
+
+	DirectX::XMFLOAT3 GetPosition();
+	DirectX::XMFLOAT3 GetRotation();
+	DirectX::XMFLOAT3 GetScale();
+	DirectX::XMFLOAT4X4 GetWorldMatrix();
+
+private:
+	// Raw transformation data
+	DirectX::XMFLOAT3 position;
+	DirectX::XMFLOAT3 pitchYawRoll;
+	DirectX::XMFLOAT3 scale;
+
+	// Finalized matrix
+	DirectX::XMFLOAT4X4 worldMatrix;
+
+};
